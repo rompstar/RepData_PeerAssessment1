7a4cd04cd9088e3f8b7498e9b0a67432a365361d
Ray Miecznik <rompstar@gmail.com>
2017-04-21 20:37:04 -0700

    Delete conver_script.R

--- conver_script.R
+++
@@ -1,7 +0,0 @@
-getwd()
-
-#transform the .Rmd to a markdown (.md) file.
-knit('PA1_template.Rmd')
-
-#transform the .md to HTML format
-markdownToHTML("PA1_template.md", "PA1_template.html", fragment.only = TRUE)
